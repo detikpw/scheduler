0cc55d2fec830c9ddfe07ac709c033547caa8d31
detikpw <the2ndflamingheart@gmail.com>
2016-08-31 10:17:05 +0700

    Parse json

--- main.rb
+++ main.rb
@@ -1,17 +1,18 @@
 require 'rufus-scheduler'
 require 'net/http'
 require './config.rb'
+require 'json'
 include Config
 
 scheduler = Rufus::Scheduler.new
 http = Net::HTTP
 
 # scheduler.every '1d' do
-  uri = URI(INSTAGRAM[:SERVER_URL])
+  instagram_uri = URI(INSTAGRAM[:SERVER_URL])
   api_path = INSTAGRAM[:API][:PATH]
-  uri.path = "#{api_path[:VERSION]}#{api_path[:RECENT_MEDIA]}"
-  uri.query = URI.encode_www_form({access_token: INSTAGRAM[:ACCESS_TOKEN]})
-  res = http.get_response(uri)
-  body = res.body if http.superclass.HTTPSuccess
-  puts body
+  instagram_uri.path = "#{api_path[:VERSION]}#{api_path[:RECENT_MEDIA]}"
+  instagram_uri.query = URI.encode_www_form({access_token: INSTAGRAM[:ACCESS_TOKEN]})
+  res = http.get_response(instagram_uri)
+  body = res.body if Net::HTTPSuccess
+  posts = JSON.parse(body)['data']
 # end
